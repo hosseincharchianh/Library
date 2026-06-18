d3e082102e92f9a5cf0eb47270c40043510d3882
Hossein Charchian <hosseincharchian@gmail.com>
2026-06-18 20:20:44 +0330

    1-bug fix

--- obj/Debug/net10.0/databaseExercise.AssemblyInfo.cs
+++ obj/Debug/net10.0/databaseExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("databaseExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40450cd100a7b0a9e563aaaa36907d935f1dbdf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0fc69a5bfdc475230acc9a7e5db0c5d30a2087")]
 [assembly: System.Reflection.AssemblyProductAttribute("databaseExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("databaseExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
